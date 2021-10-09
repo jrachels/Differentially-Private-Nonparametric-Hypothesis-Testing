3d4d7a5f718b68fe750231e7e425460a9a7d705d
jrachels <92210470+jrachels@users.noreply.github.com>
2021-10-09 07:44:30 -0400

    Add files via upload

---
+++ DifferentiallyPrivateNonparametricHypthothesisTesting/Include/dpnht.h
@@ -0,0 +1,7 @@
+// @author Jeremy Rachels
+// Contact: jeremyrachels@gmail.com
+
+#include "DifferentiallyPrivateNonparametricHypthothesisTesting/laplacedistribution/laplacedistribution.h"
+#include "DifferentiallyPrivateNonparametricHypthothesisTesting/privatekruskalwallis/privatekruskalwallis.h"
+#include "DifferentiallyPrivateNonparametricHypthothesisTesting/privatemannwhitney/privatemannwhitney.h"
+#include "DifferentiallyPrivateNonparametricHypthothesisTesting/privatewilcoxon/privatewilcoxon.h"
